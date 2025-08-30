6ce681b9a55f190cf78be7f101a0333544461cff
thatladytrucker <thatladytrucker@gmail.com>
2025-08-30 14:22:33 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles.css";
 
-// vite-plugin-pwa will auto-register the service worker
 createRoot(document.getElementById("root")).render(<App />);
